966a0e64ca784683a8dc46f34da581df0827850c
Max Anderberg <maxanderberg@gmail.com>
2022-03-02 21:03:07 +0100

    added a goodbye handler

--- go/src/github.com/maxanderberg/simple-service/main.go
+++ go/src/github.com/maxanderberg/simple-service/main.go
@@ -11,9 +11,10 @@ func main() {
 
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
-
+	gh := handlers.NewGoodBye(l)
 	sm := http.NewServeMux()
 	sm.Handle("/", hh) // Handle is like a router? Not sure... Need to find this out
+	sm.Handle("/goodbye", gh)
 	http.ListenAndServe(":9090", sm)
 
 }
