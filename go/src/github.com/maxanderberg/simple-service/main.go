fd77323495bd5900af692c70f377555f9fe0c45c
Max Anderberg <maxanderberg@gmail.com>
2022-03-03 20:48:37 +0100

    added server configuration

--- go/src/github.com/maxanderberg/simple-service/main.go
+++ go/src/github.com/maxanderberg/simple-service/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"simple-service/handlers"
+	"time"
 )
 
 func main() {
@@ -13,8 +14,16 @@ func main() {
 	hh := handlers.NewHello(l)
 	gh := handlers.NewGoodBye(l)
 	sm := http.NewServeMux()
-	sm.Handle("/", hh) // Handle is like a router? Not sure... Need to find this out
+	sm.Handle("/", hh) // Handle is like a end point, when end point / is fired do hh.
 	sm.Handle("/goodbye", gh)
-	http.ListenAndServe(":9090", sm)
 
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	s.ListenAndServe()
 }
