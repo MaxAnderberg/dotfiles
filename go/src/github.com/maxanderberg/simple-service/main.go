4608fdf13ac075a1de77a33b095582cad5b56728
Max Anderberg <maxanderberg@gmail.com>
2022-03-03 21:13:56 +0100

    added graceful shutdown

--- go/src/github.com/maxanderberg/simple-service/main.go
+++ go/src/github.com/maxanderberg/simple-service/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"simple-service/handlers"
 	"time"
 )
@@ -25,5 +27,21 @@ func main() {
 		WriteTimeout: 1 * time.Second,
 	}
 
-	s.ListenAndServe()
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+
+	l.Println("Recieved terminate, graceful shutdown", sig)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+
+	s.Shutdown(tc)
 }
