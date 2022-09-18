9155e1069ea35f6f9af9a33f595324bcb2fdf3cb
Max Anderberg <maxanderberg@gmail.com>
2022-09-18 20:46:58 +0200

    added doom emacs config

---
+++ .doom.d/custom.el
@@ -0,0 +1,25 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-idle-delay 0)
+ '(company-insertion-triggers '(32 95 40 41 46 36 60))
+ '(flycheck-inline-mode-hook '(flycheck-inline-mode-set-explicitly))
+ '(flyspell-delay 3)
+ '(lsp-headerline-breadcrumb-enable t)
+ '(lsp-modeline-code-actions-enable t)
+ '(lsp-ui-doc-delay 0.1)
+ '(lsp-ui-doc-position 'at-point)
+ '(lsp-ui-doc-show-with-cursor t)
+ '(lsp-ui-sideline-enable t)
+ '(lsp-ui-sideline-show-code-actions t)
+ '(lsp-ui-sideline-show-diagnostics t)
+ '(lsp-ui-sideline-show-hover nil)
+ '(which-key-idle-delay 0.2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
